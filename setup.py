919d07daba529360a7b71f8c8920b0059d63da11
Aaron Janse <gitduino@gmail.com>
2017-07-13 12:30:20 -0700

    stop using README.rst; only use README.md

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 from setuptools import setup
-
-with open('README.rst', 'r') as f:
-    long_description = f.read()
-
 setup(
     name='stickbugml',
     version='1.0',
     description='A framework to organize the process of designing supervised machine learning systems',
     keywords='stick bug, ml, machine learning, ai, artificial intelligence, framework, organization, organize',
-    long_description=long_description,
     license="Apache 2.0",
     author='Aaron Janse',
     author_email='gitduino@gmail.com',
